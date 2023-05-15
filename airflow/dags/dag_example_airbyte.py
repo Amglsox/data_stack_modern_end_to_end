52f87ce43462dd90099932e00e21374f67c9ea4b
Lucas Mari <lucashrm97@outlook.com>
2023-05-15 11:37:13 -0300

    feat: add new operators

--- airflow/dags/dag_example_airbyte.py
+++ airflow/dags/dag_example_airbyte.py
@@ -1,7 +1,11 @@
 from airflow import DAG
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.utils.dates import days_ago
+
+
+EXECUTION_DATE_INIT = "{{ dag_run.logical_date.strftime('Y_%m_%d') }}"
 
 
 with DAG(
@@ -12,16 +16,36 @@ with DAG(
 ) as dag:
 
     async_money_to_json = AirbyteTriggerSyncOperator(
-        task_id="airbyte_async_money_json_example",
+        task_id="airbyte_async_data_vendas",
         airbyte_conn_id="airbyte_conn_example",
         connection_id="1e3b5a72-7bfd-4808-a13c-204505490110",
         asynchronous=True,
     )
 
     airbyte_sensor = AirbyteJobSensor(
-        task_id="airbyte_sensor_money_json_example",
+        task_id="airbyte_sensor_data_vendas",
         airbyte_conn_id="airbyte_conn_example",
         airbyte_job_id=async_money_to_json.output,
+    )
+    GCSToBigQueryOperator(
+        task_id="gcs_to_bq_job_bronze_tb_vendas",
+        bucket="lucas-datalake-transient",
+        source_objects=f"shopping/tb_vendas/{EXECUTION_DATE_INIT}*.parquet",
+        destination_project_dataset_table="shopping_bronze",
+        source_format="PARQUET",
+        create_disposition="CREATE_IF_NEEDED",
+        write_disposition="WRITE_APPEND",
+        autodetect=True,
+    )
+    GCSToBigQueryOperator(
+        task_id="gcs_to_bq_job_bronze_tb_customer",
+        bucket="lucas-datalake-transient",
+        source_objects=f"shopping/tb_customer/{EXECUTION_DATE_INIT}*.parquet",
+        destination_project_dataset_table="shopping_bronze",
+        source_format="PARQUET",
+        create_disposition="CREATE_IF_NEEDED",
+        write_disposition="WRITE_APPEND",
+        autodetect=True,
     )
 
     async_money_to_json >> airbyte_sensor
